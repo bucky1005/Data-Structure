b73d0d76edca7620ae1b8660ba29b3744c520be2
rimse <null00007@naver.com>
2023-06-02 13:40:49 +0900

    Edit array elements

---
+++ BubbleSortEx/BubbleSortEx/main.c
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+#define _CRT_SECURE_NO_WARNINGS
+#pragma warning(disable:4996)
+
+#define MAX_SIZE 10 // 배열의 크기를 최대 10으로 설정
+
+int list[MAX_SIZE] = {5, 3, 8, 4, 7, 6, 10, 1, 21, 18}; // 크기가 10인 배열 생성
+int n; // 전역 변수 n 생성
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
--- InsertionSortEx/InsertionSortEx/main.c
+++ InsertionSortEx/InsertionSortEx/main.c
@@ -6,7 +6,7 @@
 
 #define MAX_SIZE 10 // 배열의 크기를 최대 10으로 설정
 
-int list[MAX_SIZE] = {5, 3, 8, 4, 7, 6, 10, 1, 21, 18}; // 크기가 10인 배열 생성
+int list[MAX_SIZE]; // 크기가 10인 배열 생성
 int n; // 전역 변수 n 생성
 
 void insertion_sort(int list[], int n){ // 배열과 배열요소 n을 인수로 받는 삽입 함수 정의
@@ -22,9 +22,9 @@ void insertion_sort(int list[], int n){ // 배열과 배열요소 n을 인수로
 int main(void){
     int i; // 반복문에 사용할 인자 선언
     n = MAX_SIZE; // 배열 요소 n을 배열의 최대 크기로 설정
-//    srand(time(NULL)); // 현재 시간에 따라 변하는 난수 생성
-//    for(i=0; i<n;i++) // 배열에 값을 입력하는 반복문
-//        list[i] = rand() % 100; // 생성된 난수의 10의 자리까지 잘라 배열에 저장
+    srand(time(NULL)); // 현재 시간에 따라 변하는 난수 생성
+    for(i=0; i<n;i++) // 배열에 값을 입력하는 반복문
+        list[i] = rand() % 100; // 생성된 난수의 10의 자리까지 잘라 배열에 저장
     
     insertion_sort(list, n); // 삽입 정렬 함수 호출
     for(i=0;i<n;i++) printf("%d ", list[i]); // 배열을 0번부터 순차적으로 출력
