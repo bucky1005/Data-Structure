0fe1fc0d6917c99bb3bbc0ffcf3f70583a87c528
rimse <null00007@naver.com>
2023-06-14 22:46:42 +0900

    add QuickSortEx

--- QuickSortEx01/QuickSortEx01/main.c
+++ QuickSortEx01/QuickSortEx01/main.c
@@ -9,36 +9,35 @@
 
 int list[MAX_SIZE];
 int n;
-int partition(int list[], int left, int right);
 
-void quick_sort(int list[], int left, int right){
-    if(left<right) {
-        int q = partition(list, left, right);
-        quick_sort(list, left, q-1);
-        quick_sort(list, q + 1, right);
-    }
-}
-
-int partition(int list[], int left, int right){
-    int pivot, temp;
-    int low, high;
+int partition(int list[], int left, int right){ //퀵정렬을 위한 분할 함수 생성
+    int pivot, temp; // 기준점과 임시 변수 선언
+    int low, high; //부분 배열 인덱스 선언
     
-    low=left;
-    high=right+1;
-    pivot=list[left];
+    low=left; //좌측 부분 배열 인덱스
+    high=right+1; //우측 부분 배열 인덱스
+    pivot=list[left]; //기준점을 왼쪽 인덱스의 요소로 선언
     
     do {
         do
-            low++;
-        while (list[low] < pivot);
+            low++; // low를 증가
+        while (list[low] < pivot); // 좌측 배열에서 pivot보다 작은 값이 나올 때까지
         do
-            high--;
-        while(list[high] > pivot);
-        if(low<high) SWAP(list[low], list[high], temp);
-    }while (low < high);
+            high--; //high를 감소
+        while(list[high] > pivot); // 우측 배열에서 pivot보다 큰 값이 나올 때까지
+        if(low<high) SWAP(list[low], list[high], temp); // 피벗을 기준으로 좌측에는 작은값, 우측에는 큰값이 오도록 교환
+    }while (low < high); //low값이 high보다 작을동안 반복
     
-    SWAP(list[left], list[high], temp);
-    return high;
+    SWAP(list[left], list[high], temp); // 피벗을 제자리에 위치
+    return high; //high값을 반환하여 피벗의 위치를 알림
+}
+
+void quick_sort(int list[], int left, int right){ 
+    if(left<right) { //정렬할 부분 배열의 크기가 1이상인 경우
+        int q = partition(list, left, right); // 분할함수를 호출하여 피벗을 중심으로 좌측, 우측 배열 분할
+        quick_sort(list, left, q-1); // 피벗을 기준으로 좌측 배열 정렬
+        quick_sort(list, q + 1, right); // 피벗을 기준으로 우측 배열 정렬
+    }
 }
 
 int main(void) {
